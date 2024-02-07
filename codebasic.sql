92db611860a562b719068262c1e20fa22ca61500
Github-sanket07sett <137095374+Github-sanket07sett@users.noreply.github.com>
2024-02-07 10:16:49 +0530

    Update codebasic.sql

--- codebasic.sql
+++ codebasic.sql
@@ -86,6 +86,57 @@ from `retail_events_db`.`fact_events`
 group by promo_type
 order by IR desc;
 
+#Total revenue
+ SELECT sum(base_price*`quantity_sold(before_promo)`)+sum(base_price*`quantity_sold(after_promo)`) as total_revenue
+ FROM `retail_events_db`.`fact_events` inner join dim_campaigns 
+on dim_campaigns.campaign_id= `retail_events_db`.`fact_events`.campaign_id ;
+
+# Total revenue by each city
+ SELECT sum(base_price*`quantity_sold(before_promo)`)+sum(base_price*`quantity_sold(after_promo)`) as total_revenue,city
+ FROM `retail_events_db`.`fact_events` inner join dim_stores 
+ on dim_stores.store_id=`retail_events_db`.`fact_events`.store_id group by city order by total_revenue desc;
+
+#Incremental Revenue ratio(incremental_revenue/total revenue)
+ select (sum(base_price*`quantity_sold(after_promo)`)-sum(base_price*`quantity_sold(before_promo)`))/(sum(base_price*`quantity_sold(before_promo)`)+sum(base_price*`quantity_sold(after_promo)`)) 
+ as Incremental_revenue_ratio
+ FROM `retail_events_db`.`fact_events`;
+ 
+ #IRPU
+select (sum(base_price*`quantity_sold(after_promo)`)-sum(base_price*`quantity_sold(before_promo)`))/((sum(`quantity_sold(after_promo)`)-sum(`quantity_sold(before_promo)`))/sum(`quantity_sold(before_promo)`))*100
+AS INCREMENTAL_REVENUE_PER_UNIT
+FROM `retail_events_db`.`fact_events`;  
+
+#ARPU
+SELECT (sum(base_price*`quantity_sold(before_promo)`)+sum(base_price*`quantity_sold(after_promo)`))/(sum(`quantity_sold(after_promo)`)+sum(`quantity_sold(before_promo)`))
+AS AVERAGE_REVENUE_PER_UNIT
+FROM `retail_events_db`.`fact_events`;  
+
+# NO. OF UNITS SOLD BY EACH CATEGORY
+SELECT (sum(`quantity_sold(after_promo)`)+sum(`quantity_sold(before_promo)`)) AS UNIT_SOLD,category
+FROM `retail_events_db`.`fact_events` INNER JOIN dim_products
+on dim_products.product_code=`retail_events_db`.`fact_events`.product_code 
+group by category;
+
+#INCREMENTAL REV BY EACH CATEGORY
+ select (sum(base_price*`quantity_sold(after_promo)`)-sum(base_price*`quantity_sold(before_promo)`)) AS IR,category
+FROM `retail_events_db`.`fact_events` INNER JOIN dim_products
+on dim_products.product_code=`retail_events_db`.`fact_events`.product_code 
+group by category 
+ORDER BY IR DESC;
+
+#total revenue by each category
+SELECT sum(base_price*`quantity_sold(before_promo)`)+sum(base_price*`quantity_sold(after_promo)`) as total_revenue,category
+FROM `retail_events_db`.`fact_events` INNER JOIN dim_products
+on dim_products.product_code=`retail_events_db`.`fact_events`.product_code 
+group by category 
+ORDER BY total_revenue DESC;
+
+# ISU BY EACH CATEGORY
+select ((sum(`quantity_sold(after_promo)`)-sum(`quantity_sold(before_promo)`))/sum(`quantity_sold(before_promo)`))*100 as ISU,category
+FROM `retail_events_db`.`fact_events`INNER JOIN dim_products
+on dim_products.product_code=`retail_events_db`.`fact_events`.product_code 
+group by category 
+ORDER BY ISU DESC;
 
 
  
